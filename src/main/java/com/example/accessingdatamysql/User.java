c1e314a20807a9aac4e86454cee171241366fdad
Carl Wilson <carl.wilson@nickabbey.co.uk>
2024-01-10 16:36:16 +0000

    New tests

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -1,40 +1,33 @@
 package com.example.accessingdatamysql;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
+    @Column(name = "name", nullable = false)
     private String name;
-
+    @Column(name = "email", nullable = false)
     private String email;
 
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
--- src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
+++ src/test/java/com/example/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.accessingdatamysql;
 
+import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class AccessingDataMysqlApplicationTests {
 
+	@Autowired
+	private UserRepository r;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAdd() {
+		User u = new User();
+		u.setName("John");
+		u.setEmail("john@aol.com");
+		r.save(u);
+		Assertions.assertThat(u.getId()).isGreaterThan(0);
 	}
 
 }
